e33d7c0c38727cad8853d8709790fb162bb15934
Mahender Kumar <mksb1947@gmail.com>
2024-07-20 12:53:36 +0530

    scanner working

--- lib/generator.dart
+++ lib/generator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class Generator extends StatefulWidget {
   const Generator({super.key});
@@ -8,8 +9,61 @@ class Generator extends StatefulWidget {
 }
 
 class _GeneratorState extends State<Generator> {
+  TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Generate QR Code'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              if (controller.text.isNotEmpty)
+                // Image.network(
+                //   'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${controller.text}',
+                // ),
+                Expanded(
+                  child: Center(
+                    child: SizedBox(
+                      width: 280,
+                      child: QrImageView(
+                        data: controller.text,
+                        version: QrVersions.auto,
+                        eyeStyle: const QrEyeStyle(
+                          eyeShape: QrEyeShape.square,
+                          color: Color(0xff128760),
+                        ),
+                        dataModuleStyle: const QrDataModuleStyle(
+                          dataModuleShape: QrDataModuleShape.square,
+                          color: Color(0xff1a5441),
+                        ),
+                        embeddedImage: const AssetImage(
+                            'assets/images/4.0x/logo_yakka_transparent.png'),
+                        embeddedImageStyle: const QrEmbeddedImageStyle(
+                          size: Size.square(40),
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              TextField(
+                controller: controller,
+                decoration: const InputDecoration(
+                  hintText: 'Enter the text to generate QR Code',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {});
+                },
+                child: const Text('Generate QR Code'),
+              ),
+            ],
+          ),
+        ));
   }
 }
--- lib/scanner.dart
+++ lib/scanner.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class Scanner extends StatefulWidget {
   const Scanner({super.key});
@@ -8,8 +9,42 @@ class Scanner extends StatefulWidget {
 }
 
 class _ScannerState extends State<Scanner> {
+  String qr = '';
+  Future<void> scanQRCode() async {
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      final qrCode = await FlutterBarcodeScanner.scanBarcode(
+        '#ff6666',
+        'Cancel',
+        true,
+        ScanMode.QR,
+      );
+      if (!mounted) return;
+      setState(() {
+        qr = qrCode.toString();
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Scan QR Code'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(qr),
+              ElevatedButton(
+                onPressed: scanQRCode,
+                child: const Text('Scan QR Code'),
+              ),
+            ],
+          ),
+        ));
   }
 }
